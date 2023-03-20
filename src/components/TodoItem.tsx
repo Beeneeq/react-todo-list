309ab7707952c10548f3f595ebb0e04496239a2c
Beeneeq <Beeneeq>
2023-03-20 20:27:36 +0900

    remove 기능 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Todo, useTodoDispatch } from '../contexts/TodosContext'
-import { MdDone } from 'react-icons/md';
+import { MdDone, MdDelete } from 'react-icons/md';
 
 const TodoItem = ({text, done, id}: Todo) => {  
-  const dispatch: any = useTodoDispatch();
-  const onToggle = () => dispatch({ type: 'TOGGLE', id });
+  const dispatch: any = useTodoDispatch()
+  const onToggle = () => dispatch({ type: 'TOGGLE', id })
+  const onDelete = () => dispatch({ type: 'REMOVE', id })
+  const onMouseOver = () => {
+    // 추후 수정
+  }
 
   return (
-    <li className={done ? 'flex2 todo-item done' : 'flex2 todo-item'} key={id}>
+    <li className={done ? 'flex2 todo-item done' : 'flex2 todo-item'} key={id} onMouseOver={onMouseOver}>
         {text}
-        <div className='btn-check' onClick={onToggle}>
-          <MdDone/>
+        <div className='flex'>
+          <div className='btn-check' onClick={onToggle}>
+            <MdDone/>
+          </div>
+          <div className='btn-del' onClick={onDelete} style={{marginLeft: '10px'}}>          
+            <MdDelete/>
+          </div>
         </div>
     </li>
   )
